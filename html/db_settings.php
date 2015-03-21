8c431311a10a6ff282ba182f38c7048c3cfb7c34
nihilianth <nihilianth@github.com>
2015-03-21 20:33:47 +0100

    tolles design

--- html/db_settings.php
+++ html/db_settings.php
@@ -2,7 +2,7 @@
 
 
 define("HOST", "localhost");     // The host you want to connect to.
-define("USER", "root");    // The database username.
+define("USER", "root2");    // The database username.
 define("PASSWORD", "");    // The database password.
 define("DATABASE", "webshop");    // The database name.
 
