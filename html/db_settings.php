5fd4376aae10aafc2b54c009cce8d8382be4a3f2
vollniki <niklas.meusel@web.de>
2015-03-21 14:10:20 +0100

    Datenbank erweitert
    db username auf root geändert und Datenbank example erweitert

--- html/db_settings.php
+++ html/db_settings.php
@@ -2,8 +2,8 @@
 
 
 define("HOST", "localhost");     // The host you want to connect to.
-define("USER", "root2");    // The database username. 
-define("PASSWORD", "");    // The database password. 
+define("USER", "root");    // The database username.
+define("PASSWORD", "");    // The database password.
 define("DATABASE", "webshop");    // The database name.
- 
+
 ?>
